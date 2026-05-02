15fcc215312fa0aba09124804e1a5ec8d1d86a7f
akaf-auto <akaf-auto@example.com>
2026-05-02 15:56:58 +0400

    Move local SQLite database outside project folder

--- core/settings.py
+++ core/settings.py
@@ -30,7 +30,7 @@ elif ENVIRONMENT == 'local':
         DATABASES = {
             'default': {
                 'ENGINE': 'django.db.backends.sqlite3',
-                'NAME': BASE_DIR / 'db.sqlite3',
+                'NAME': BASE_DIR.parent / 'db.sqlite3',
             }
         }
         MEDIA_URL = '/media/'
